88fcfc5d474096f04d4cacfad8685b1be3e73811
DanielPuente <puente10@hotmail.com>
2023-04-01 21:02:31 -0600

    Initial Commit

---
+++ AppPruebaGIT/AppPruebaGITApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPruebaGITApp.swift
+//  AppPruebaGIT
+//
+//  Created by Daniel Puente on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AppPruebaGITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
